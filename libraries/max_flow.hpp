9d58833edfe6910ef0067458d2792b5a9ef0f3d0
mainyutin <engineerxl@yahoo.com>
2025-02-19 16:05:53 +0300

    Added flow decomposition

--- libraries/max_flow.hpp
+++ libraries/max_flow.hpp
@@ -20,12 +20,12 @@ struct flow_network_t {
     };
 
     int n, source, sink;
-    vi d, ptr;
+    vi d, ptr, vis, path;
     std::vector<vi> g;
     std::vector<edge_t> e;
     static const ll INF = 1e18;
 
-    flow_network_t(int n) : n(n), d(n), ptr(n), g(n) {}
+    flow_network_t(int n) : n(n), d(n), ptr(n), vis(n), g(n) {}
 
     // Adds directed edge u -> v with capacity c
     void add_edge(int u, int v, ll c) {
@@ -36,6 +36,7 @@ struct flow_network_t {
     }
 
     // Finds the maximum flow from vertex s to vertex t
+    // Complexity is O(|V| * |E| * log(c_max)) in general case
     ll max_flow(int s, int t) {
         source = s, sink = t;
         clear_flow();
@@ -43,6 +44,50 @@ struct flow_network_t {
         for (auto [u, v, c, f] : e) max_c = std::max(max_c, c);
         for (ll b = max_c; b > 0; b >>= 1) res += dinic(b);
         return res;
+    }
+
+    bool dfs(int u) {
+        vis[u] = 1;
+        if (u == sink) return true;
+        for (int j : g[u]) {
+            auto [_, v, c, f] = e[j];
+            if (f <= 0) continue;
+            path.push_back(j);
+            if (vis[v]) {
+                for (int i = 0; i < (int)path.size(); ++i) {
+                    if (e[path[i]].u == v) {
+                        path = {path.begin() + i, path.end()};
+                        return true;
+                    }
+                }
+            }
+            if (dfs(v)) return true;
+            path.pop_back();
+        }
+        return false;
+    }
+
+    // Decompose flow on simple paths (invalidates the flow value on edges)
+    // Return vector of paths and vector of flow values along paths
+    // f_max restricts summary flow value to decompose
+    // Complexity is O(|V| * |E|)
+    std::pair<std::vector<vi>, std::vector<ll>> decompose(ll f_max = INF) {
+        std::vector<vi> res;
+        std::vector<ll> f;
+        while (f_max > 0 && dfs(source)) {
+            ll mn_f = INF;
+            for (int j : path) mn_f = std::min(mn_f, e[j].f);
+            if (e[path.back()].v == sink) {
+                res.emplace_back(1, source);
+                for (int j : path) res.back().push_back(e[j].v);
+                f.push_back(mn_f);
+                f_max -= mn_f;
+            }
+            for (int j : path) e[j].f -= mn_f;
+            vis.assign(n, 0);
+            path.clear();
+        }
+        return {res, f};
     }
 
     friend std::ostream& operator<<(std::ostream& out,
