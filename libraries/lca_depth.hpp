37e23004ab4bd51f00bd64dee14b90416d6d1e02
mainyutin <engineerxl@yahoo.com>
2025-08-12 13:02:16 +0300

    Better depth lca

--- libraries/lca_depth.hpp
+++ libraries/lca_depth.hpp
@@ -1,77 +1,82 @@
-#ifndef LCA_HPP
-#define LCA_HPP
+#ifndef LCA_DEPTH_HPP
+#define LCA_DEPTH_HPP
 
+#include <array>
 #include <vector>
 
-struct edge_t {
-    int u, v;
-};
+/*
+ * Simple LCA struct, supports leaf addition
+ * T is an edge type, modify dfs to match it
+ * By default dfs works with struct (v_to, e_weight)
+ */
+template <class T>
+struct lca_t {
+    static const int L = 20;
+    using ar = std::array<int, L>;
+    using graph = std::vector<std::vector<T> >;
 
-using graph_t = std::vector<std::vector<edge_t> >;
-
-class lca_t {
-    static const int MAX_DEPTH = 20;
-
-    size_t n;
-    std::vector<std::vector<int> > up;
+    int n;
+    std::vector<ar> up;
     std::vector<int> d;
 
-    void dfs(const graph_t& g, int u, int prev) {
-        for (const edge_t& elem : g[u]) {
-            int v = elem.v;
-            if (v != prev) {
-                d[v] = d[u] + 1;
-                up[v][0] = u;
-                dfs(g, v, u);
-            }
+    void dfs(const graph& g, int u, int prev) {
+        for (const auto& [v, _] : g[u]) {
+            if (v == prev) continue;
+            d[v] = d[u] + 1;
+            up[v][0] = u;
+            dfs(g, v, u);
         }
     }
 
-   public:
-    lca_t(const graph_t& g, int root) : n(g.size()) {
-        up.resize(n, std::vector<int>(MAX_DEPTH));
-        d.resize(n);
-        d[root] = 0;
-        up[root][0] = root;
-        dfs(g, root, -1);
-        for (int j = 1; j < MAX_DEPTH; ++j) {
-            for (size_t u = 0; u < n; ++u) {
+    // Set root = -1 to handle forest
+    lca_t(const graph& g, int root = 0) : lca_t(g.size(), root) {
+        if (root != -1)
+            dfs(g, root, root);
+        else {
+            for (int i = 0; i < n; ++i) {
+                up[i][0] = -1;
+            }
+            for (int i = 0; i < n; ++i) {
+                if (up[i][0] != -1) continue;
+                up[i][0] = i;
+                dfs(g, i, i);
+            }
+        }
+        for (int j = 1; j < L; ++j) {
+            for (int u = 0; u < n; ++u) {
                 up[u][j] = up[up[u][j - 1]][j - 1];
             }
         }
     }
 
-    lca_t() : n(1) {
-        up.resize(n, std::vector<int>(MAX_DEPTH));
-        d.resize(n);
-        d[0] = 0;
+    lca_t(int n = 1, int root = 0) : n(n), up(n), d(n) {
+        if (root != -1) up[root][0] = root;
     }
 
     void add_leaf(int prev, int u) {
         ++n;
         d.push_back(d[prev] + 1);
-        up.push_back(std::vector<int>(MAX_DEPTH));
+        up.emplace_back();
         up[u][0] = prev;
-        for (int j = 1; j < MAX_DEPTH; ++j) {
+        for (int j = 1; j < L; ++j) {
             up[u][j] = up[up[u][j - 1]][j - 1];
         }
     }
 
-    /* lca(x, y) */
+    // Walk dist edges up from u
+    int lift(int u, int dist) {
+        for (int j = L - 1; j >= 0; --j) {
+            if (dist & (1 << j)) u = up[u][j];
+        }
+        return u;
+    }
+
+    // Find lca(x, y)
     int find(int x, int y) {
-        if (d[x] > d[y]) {
-            std::swap(x, y);
-        }
-        int delta = d[y] - d[x];
-        for (int j = MAX_DEPTH - 1; j >= 0; --j) {
-            if (delta & (1 << j)) {
-                y = up[y][j];
-            }
-        }
-        if (x == y) {
-            return x;
-        }
-        for (int j = MAX_DEPTH - 1; j >= 0; --j) {
+        if (d[x] > d[y]) std::swap(x, y);
+        y = lift(y, d[y] - d[x]);
+        if (x == y) return x;
+        for (int j = L - 1; j >= 0; --j) {
             if (up[x][j] != up[y][j]) {
                 x = up[x][j];
                 y = up[y][j];
@@ -79,31 +84,6 @@ class lca_t {
         }
         return up[x][0];
     }
-
-    /* path length from x to y */
-    int dist(int x, int y) {
-        if (d[x] > d[y]) {
-            std::swap(x, y);
-        }
-        int delta = d[y] - d[x];
-        for (int j = MAX_DEPTH - 1; j >= 0; --j) {
-            if (delta & (1 << j)) {
-                y = up[y][j];
-            }
-        }
-        int res = delta;
-        if (x == y) {
-            return res;
-        }
-        for (int j = MAX_DEPTH - 1; j >= 0; --j) {
-            if (up[x][j] != up[y][j]) {
-                res = res + 2 * (1 << j);
-                x = up[x][j];
-                y = up[y][j];
-            }
-        }
-        return res + 2;
-    }
 };
 
-#endif /* LCA_HPP */
+#endif /* LCA_DEPTH_HPP */
